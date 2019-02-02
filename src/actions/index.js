f48a61c2e82d2049c15e73bf5d0e4a470dfb1083
hellomac87 <hellomac87@gmail.com>
2019-02-03 00:26:59 +0900

    reducer state immutable

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,10 +7,8 @@ export const fetchWeather = city => {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
 
-  console.log("Request", request);
-
   return {
     type: FETCH_WEATHER,
-    payload: request
+    payload: request.data
   };
 };
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,12 @@
-export const weather = (state = null, action) => {
-  console.log("action received", action);
+import { FETCH_WEATHER } from "../actions/index";
+
+export const weather = (state = [], action) => {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // return state.push(action.payload.data); // wrong way
+      // return state.concat([action.payload.data]); // old way
+      return [action.payload.data, ...state]; // es6 way
+  }
+
   return state;
 };
