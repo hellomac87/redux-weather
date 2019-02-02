be65e653b0efd85a0b2f1f3939af543a9610fc58
hellomac87 <hellomac87@gmail.com>
2019-02-02 11:12:27 +0900

    feat: axios, action creator add

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const API_KEY = "90abba351bf4321006802ca770d59853";
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export const fetchWeather = city => {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+};
